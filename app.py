50e8d261b5ec923f77e088c3ef0e43c942b9022c
kparbin774-max <kparbin774@gmail.com>
2026-09-09 15:02:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,3 +176,13 @@ def logout():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
